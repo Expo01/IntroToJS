af5deb60ca2d0bf673e57f6017f4f3b941890f02
Ryan <crdailey01@gmail.com>
2024-03-07 22:13:17 -0800

    HTML + CSS + JS

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function externalFunction(){
+    document.getElementById("demo3").innerHTML = "Wizz.";
+}
+function externalFunction2(){
+    document.getElementById("demo3").innerHTML = "No wizz.";
+}
+
+let num = 1;
+let num2 = 2;
+let result = num + num2;
+console.log(result);
